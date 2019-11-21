66ecd5273505e6ed2310590b126e32b6600008cf
Michael Harris <mhguitar1997@gmail.com>
2019-11-21 11:43:52 -0800

    Change colors on buttons

--- adventure/src/Components/World.js
+++ adventure/src/Components/World.js
@@ -1,39 +1,54 @@
-import React from 'react';
-import styled from 'styled-components';
-import { axiosWithAuth } from '../utils/axiosWithAuth';
+import React from "react";
+import styled, {css} from "styled-components";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-import vertPath from '../assets/vertical-path.png';
-import horizPath from '../assets/horizontal-path.png';
-import wall from '../assets/wall.png';
-import whitespaceFiller from '../assets/whitespace.png';
-import hero from '../assets/hero.png';
-import room from '../assets/rooms.png';
+import vertPath from "../assets/vertical-path.png";
+import horizPath from "../assets/horizontal-path.png";
+import wall from "../assets/wall.png";
+import whitespaceFiller from "../assets/whitespace.png";
+import hero from "../assets/hero.png";
+import room from "../assets/rooms.png";
+
 
 const Container = styled.div``;
 const Header = styled.div``;
-const Title = styled.h1``;
-const NorthButton = styled.button`
-  display: block;
-  margin: 0 auto 20px;
-`;
-const SouthButton = styled.button`
-  display: block;
-  margin: 20px auto 0;
+const Title = styled.h1`
+  text-align: center
 `;
 
+const Button = styled.button`
+  padding: 10px 20px 10px 20px;
+  border-radius: 5px;
+  border-color: black;
+  border-width: 2px;
+  text-align: center;
+  font-weight: bolder;
+  font-size: 17px;
+  background: white;
+  color: palevioletred;
+  margin-bottom: 20px;
+  ${props => props.north && css`
+  display: block;
+  margin: 0 auto 20px;
+`}
+  ${props => props.south && css`
+  display: block;
+  margin: 0 auto 20px;
+`}
+`;
 // Change to http://127.0.0.1:8000 for local testing, https://cs23-mud.herokuapp.com for deployed server
-const baseURL = 'https://cs23-mud.herokuapp.com';
+const baseURL = "https://cs23-mud.herokuapp.com";
 
 class World extends React.Component {
   constructor(props) {
     super(props);
-    this.token = localStorage.getItem('token');
+    this.token = localStorage.getItem("token");
     this.state = {
       mapOpen: false,
-      title: '',
-      description: '',
-      error_msg: '',
-      world: ''
+      title: "",
+      description: "",
+      error_msg: "",
+      world: ""
     };
   }
 
@@ -97,18 +112,23 @@ class World extends React.Component {
       if (i > 0 && i % width === 0) {
         worldMap.push(<br key={`br ${i / width}`} />);
       }
-      if (worldString[i] === ' ') {
+
+      // worldMap.push(<img src={hero}/>)
+      if (worldString[i] === " ") {
         worldMap.push(<img key={i} src={whitespaceFiller} />);
-      } else if (worldString[i] === '#') {
+      } else if (worldString[i] === "#") {
         worldMap.push(<img key={i} src={wall} />);
-      } else if (worldString[i] === '|') {
+      } else if (worldString[i] === "|") {
         worldMap.push(<img key={i} src={vertPath} />);
-      } else if (worldString[i] === '-') {
+      } else if (worldString[i] === "-") {
         worldMap.push(<img key={i} src={horizPath} />);
-      } else if (worldString[i] === '\n') {
+      } else if (worldString[i] === "\n") {
       } else {
+        console.log("ascii", i, worldString[i]);
         worldMap.push(<img key={i} src={room} />);
       }
+      // if(worldMap)
+      // worldMap[0] = <img key={i} src={hero} />
     }
     return worldMap;
   }
@@ -116,7 +136,7 @@ class World extends React.Component {
   render() {
     let width = 0;
     for (let i = 0; i < this.state.world.length; i++) {
-      if (this.state.world[i] === '#' || this.state.world[i] === ' ') {
+      if (this.state.world[i] === "#" || this.state.world[i] === " ") {
         width += 1;
       } else {
         break;
@@ -129,29 +149,32 @@ class World extends React.Component {
           <Title>MUD Adventure!</Title>
         </Header>
 
-        <NorthButton onClick={() => this.submitMove('n')}>
+      <Movement>
+        <Button north onClick={() => this.submitMove("n")}>
           Head North
-        </NorthButton>
-        <Container style={{ display: 'flex' }}>
-          <button onClick={() => this.submitMove('w')}>Head West</button>
-          <Container style={{ margin: '0 auto' }}>
-            <p>{this.state.title || ''}</p>
-            <p>{this.state.description || 'Loading...'}</p>
-            <p style={{ color: 'red' }}>{this.state.error_msg || ''}</p>
+        </Button>
+        <Container style={{ display: "flex" }}>
+          <Button onClick={() => this.submitMove("w")}>Head West</Button>
+          <Container style={{ margin: "0 auto" }}>
+            <p>{this.state.title || ""}</p>
+            <p>{this.state.description || "Loading..."}</p>
+            <p style={{ color: "red" }}>{this.state.error_msg || ""}</p>
           </Container>
-          <button onClick={() => this.submitMove('e')}>Head East</button>
+          <Button onClick={() => this.submitMove("e")}>Head East</Button>
         </Container>
-        <SouthButton onClick={() => this.submitMove('s')}>
+        <Button south onClick={() => this.submitMove("s")}>
           Head South
-        </SouthButton>
-        <Container style={{ margin: '0 20%' }}>
+        </Button>
+        </Movement>
+        <Container style={{ margin: "0 20%" }}>
           {this.state.mapOpen ? (
-            <button onClick={() => this.setState({ mapOpen: false })}>
+            <Button onClick={() => this.setState({ mapOpen: false })}>
               Hide Map
-            </button>
+            </Button>
           ) : (
-            <button onClick={() => this.getMap()}>Show Map</button>
+            <Button onClick={() => this.getMap()}>Show Map</Button>
           )}
+          
 
           {this.state.mapOpen ? (
             <div>{this.buildMap(width + 1)}</div>
